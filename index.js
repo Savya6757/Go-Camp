8c39b5987faf93f584bcef486de1bb94b18b119f
savyasanchi shukla <you@example.com>
2022-02-03 00:27:23 +0530

    added mongo sanitize to prevent mongo injection

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const campgroundRoute = require("./routes/campground");
 const reviewsRoute = require("./routes/reviews");
@@ -39,6 +40,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize())
 
 const sessionConfig = {
   secret: "changethislater",
--- public/js/showMap.js
+++ public/js/showMap.js
@@ -6,7 +6,13 @@ const map = new mapboxgl.Map({
   zoom: 7, // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
-  .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h4>${campground.title}</h4><p>${campground.location}</p>`))
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${campground.title}</h4><p>${campground.location}</p>`
+    )
+  )
   .addTo(map);
