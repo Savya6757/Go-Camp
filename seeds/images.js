6f4999247dd0a68fd3e7327e13510a222ccb5c2d
savyasanchi shukla <you@example.com>
2022-02-06 19:29:36 +0530

    changed all images to webp format

--- seeds/images.js
+++ seeds/images.js
@@ -1,207 +1,208 @@
 module.exports.imagesSeed = [
+  // "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644152470/YelpCamp/camp_1_wawsvn.webp"
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043864/YelpCamp/camp_1_ecdics.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043864/YelpCamp/camp_1_ecdics.webp",
     name: "YelpCamp/camp_1_ecdics",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043865/YelpCamp/camp_3_snmsyg.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043865/YelpCamp/camp_3_snmsyg.webp",
     name: "YelpCamp/camp_3_snmsyg",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043865/YelpCamp/camp_6_aele6y.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043865/YelpCamp/camp_6_aele6y.webp",
     name: "YelpCamp/camp_6_aele6y",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043865/YelpCamp/camp_4_jssd2g.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043865/YelpCamp/camp_4_jssd2g.webp",
     name: "YelpCamp/camp_4_jssd2g",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043865/YelpCamp/camp_2_ffhxa8.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043865/YelpCamp/camp_2_ffhxa8.webp",
     name: "YelpCamp/camp_2_ffhxa8",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043865/YelpCamp/camp_11_uoskvi.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043865/YelpCamp/camp_11_uoskvi.webp",
     name: "YelpCamp/camp_11_uoskvi",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043865/YelpCamp/camp_7_ap7w00.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043865/YelpCamp/camp_7_ap7w00.webp",
     name: "YelpCamp/camp_7_ap7w00",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043865/YelpCamp/camp_9_anrjvz.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043865/YelpCamp/camp_9_anrjvz.webp",
     name: "YelpCamp/camp_9_anrjvz",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043866/YelpCamp/camp_12_sjit0l.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043866/YelpCamp/camp_12_sjit0l.webp",
     name: "YelpCamp/camp_12_sjit0l",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043866/YelpCamp/camp_8_vubwp4.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043866/YelpCamp/camp_8_vubwp4.webp",
     name: "YelpCamp/camp_8_vubwp4",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043866/YelpCamp/camp_13_hbdtbx.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043866/YelpCamp/camp_13_hbdtbx.webp",
     name: "YelpCamp/camp_13_hbdtbx",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043866/YelpCamp/camp_17_lpnyzt.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043866/YelpCamp/camp_17_lpnyzt.webp",
     name: "YelpCamp/camp_17_lpnyzt",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043866/YelpCamp/camp_16_srpjfn.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043866/YelpCamp/camp_16_srpjfn.webp",
     name: "YelpCamp/camp_16_srpjfn",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043866/YelpCamp/camp_14_utx5f5.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043866/YelpCamp/camp_14_utx5f5.webp",
     name: "YelpCamp/camp_14_utx5f5",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043865/YelpCamp/camp_10_zeblmm.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043865/YelpCamp/camp_10_zeblmm.webp",
     name: "YelpCamp/camp_10_zeblmm",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043865/YelpCamp/camp_5_e1ambm.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043865/YelpCamp/camp_5_e1ambm.webp",
     name: "YelpCamp/camp_5_e1ambm",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043866/YelpCamp/camp_15_eln1gw.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043866/YelpCamp/camp_15_eln1gw.webp",
     name: "YelpCamp/camp_15_eln1gw",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043866/YelpCamp/camp_18_zhlvq3.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043866/YelpCamp/camp_18_zhlvq3.webp",
     name: "YelpCamp/camp_18_zhlvq3",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_22_e2kobb.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_22_e2kobb.webp",
     name: "YelpCamp/camp_22_e2kobb",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_23_u86z8c.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_23_u86z8c.webp",
     name: "YelpCamp/camp_23_u86z8c",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_21_nfur2h.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_21_nfur2h.webp",
     name: "YelpCamp/camp_21_nfur2h",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_20_msfnu5.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_20_msfnu5.webp",
     name: "YelpCamp/camp_20_msfnu5",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_24_zcjr9e.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_24_zcjr9e.webp",
     name: "YelpCamp/camp_24_zcjr9e",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_28_xinlui.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_28_xinlui.webp",
     name: "YelpCamp/camp_28_xinlui",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_19_zyx3lr.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_19_zyx3lr.webp",
     name: "YelpCamp/camp_19_zyx3lr",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_27_atxusa.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_27_atxusa.webp",
     name: "YelpCamp/camp_27_atxusa",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_25_f45pys.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_25_f45pys.webp",
     name: "YelpCamp/camp_25_f45pys",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_29_fbsrzc.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_29_fbsrzc.webp",
     name: "YelpCamp/camp_29_fbsrzc",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_26_upisf1.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_26_upisf1.webp",
     name: "YelpCamp/camp_26_upisf1",
   },
 
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043867/YelpCamp/camp_30_gdju11.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043867/YelpCamp/camp_30_gdju11.webp",
     name: "YelpCamp/camp_30_gdju11",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043868/YelpCamp/camp_31_zny7oo.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043868/YelpCamp/camp_31_zny7oo.webp",
     name: "YelpCamp/camp_31_zny7oo",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043868/YelpCamp/camp_33_su5e7e.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043868/YelpCamp/camp_33_su5e7e.webp",
     name: "YelpCamp/camp_33_su5e7e",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043868/YelpCamp/camp_40_rsugae.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043868/YelpCamp/camp_40_rsugae.webp",
     name: "YelpCamp/camp_40_rsugae",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043868/YelpCamp/camp_35_cxerwx.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043868/YelpCamp/camp_35_cxerwx.webp",
     name: "YelpCamp/camp_35_cxerwx",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043868/YelpCamp/camp_32_f9pspb.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043868/YelpCamp/camp_32_f9pspb.webp",
     name: "YelpCamp/camp_32_f9pspb",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043868/YelpCamp/camp_36_mjxf8a.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043868/YelpCamp/camp_36_mjxf8a.webp",
     name: "YelpCamp/camp_36_mjxf8a",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043868/YelpCamp/camp_39_uw1r3o.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043868/YelpCamp/camp_39_uw1r3o.webp",
     name: "YelpCamp/camp_39_uw1r3o",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043869/YelpCamp/camp_42_ol8oca.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043869/YelpCamp/camp_42_ol8oca.webp",
     name: "YelpCamp/camp_42_ol8oca",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043868/YelpCamp/camp_34_upfm6y.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043868/YelpCamp/camp_34_upfm6y.webp",
     name: "YelpCamp/camp_34_upfm6y",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043869/YelpCamp/camp_41_c2nmmn.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043869/YelpCamp/camp_41_c2nmmn.webp",
     name: "YelpCamp/camp_41_c2nmmn",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043868/YelpCamp/camp_38_b65j6o.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043868/YelpCamp/camp_38_b65j6o.webp",
     name: "YelpCamp/camp_38_b65j6o",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043868/YelpCamp/camp_37_rfffyi.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043868/YelpCamp/camp_37_rfffyi.webp",
     name: "YelpCamp/camp_37_rfffyi",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043869/YelpCamp/camp_43_b0qlb7.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043869/YelpCamp/camp_43_b0qlb7.webp",
     name: "YelpCamp/camp_43_b0qlb7",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043869/YelpCamp/camp_49_xf4xsc.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043869/YelpCamp/camp_49_xf4xsc.webp",
     name: "YelpCamp/camp_49_xf4xsc",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043869/YelpCamp/camp_44_ae36j1.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043869/YelpCamp/camp_44_ae36j1.webp",
     name: "YelpCamp/camp_44_ae36j1",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043870/YelpCamp/camp_51_ivndn6.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043870/YelpCamp/camp_51_ivndn6.webp",
     name: "YelpCamp/camp_51_ivndn6",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043870/YelpCamp/camp_50_s7kjtf.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043870/YelpCamp/camp_50_s7kjtf.webp",
     name: "YelpCamp/camp_50_s7kjtf",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043869/YelpCamp/camp_46_he3lfc.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043869/YelpCamp/camp_46_he3lfc.webp",
     name: "YelpCamp/camp_46_he3lfc",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043869/YelpCamp/camp_47_fj89yt.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043869/YelpCamp/camp_47_fj89yt.webp",
     name: "YelpCamp/camp_47_fj89yt",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043869/YelpCamp/camp_45_k0cvjg.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043869/YelpCamp/camp_45_k0cvjg.webp",
     name: "YelpCamp/camp_45_k0cvjg",
   },
   {
-    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/v1644043869/YelpCamp/camp_48_sddtt1.jpg",
+    url: "https://res.cloudinary.com/dqtx8kikg/image/upload/q_80/v1644043869/YelpCamp/camp_48_sddtt1.webp",
     name: "YelpCamp/camp_48_sddtt1",
   },
 ];
